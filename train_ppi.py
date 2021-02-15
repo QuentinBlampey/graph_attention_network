57e1012d13a5ed7215195c62dd4f7c580c7a06a9
QuentinBlampey <quentin.blampey@gmail.com>
2021-02-15 13:44:37 +0100

    own GAT model

--- train_ppi.py
+++ train_ppi.py
@@ -1,18 +1,16 @@
 import argparse
-from os import path
-
 import numpy as np
-import torch
-import torch.nn.functional as F
 from dgl import batch
 from dgl.data.ppi import LegacyPPIDataset
 from dgl.nn.pytorch import GraphConv
 from sklearn.metrics import f1_score
+import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.utils.data import DataLoader
 
 
-MODEL_STATE_FILE = path.join(path.dirname(path.abspath(__file__)), "model_state.pth")
+MODEL_STATE_FILE = "model_state.pth"
 
 
 class BasicGraphModel(nn.Module):
@@ -22,54 +20,84 @@ class BasicGraphModel(nn.Module):
         self.g = g
         self.layers = nn.ModuleList()
         self.layers.append(GraphConv(input_size, hidden_size, activation=nonlinearity))
-        for i in range(n_layers - 1):
+        for _ in range(n_layers - 1):
             self.layers.append(GraphConv(hidden_size, hidden_size, activation=nonlinearity))
         self.layers.append(GraphConv(hidden_size, output_size))
 
     def forward(self, inputs):
         outputs = inputs
-        for i, layer in enumerate(self.layers):
+        for _, layer in enumerate(self.layers):
             outputs = layer(self.g, outputs)
         return outputs
 
-    def logits(self, features):
+    def set_graph(self, g):
         for layer in self.layers:
-            layer.g = self.g
-        return self(features)
+            layer.g = g
 
-class GraphAttentionNetwork(nn.Module):
-    def __init__(self, input_F, output_F, activation, input_slope=0.2):
+class AttentionHead(nn.Module):
+    def __init__(self, in_dim, out_dim, device, input_slope=0.2):
         super().__init__()
         self.g = None
-        self.input_F = input_F
-        self.output_F = output_F
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.device = device
         self.input_slope = input_slope
 
-        self.linear = nn.Linear(self.input_F, self.output_F)
-        self.attention = nn.Linear(2*self.output_F, 1)
+        self.linear = nn.Linear(self.in_dim, self.out_dim)
+        self.attention = nn.Linear(2*self.out_dim, 1)
 
-        self.activation = activation
+        self.init_xavier_uniform()
 
+    def init_xavier_uniform(self):
+        gain = nn.init.calculate_gain('relu')
+        nn.init.xavier_uniform_(self.linear.weight, gain=gain)
+        nn.init.xavier_uniform_(self.attention.weight, gain=gain)
 
     def forward(self, inputs):
         Wh = self.linear(inputs)
         
         n_nodes = self.g.number_of_nodes()
-        coeffs = torch.zeros((n_nodes, n_nodes))
+        U, V = self.g.all_edges()
 
-        for u,v in zip(*self.g.all_edges()):
-            coeff = self.attention(torch.cat((Wh[u], Wh[v])))
-            coeffs[u,v] = coeff
-            coeffs[v,u] = coeff
+        edges_repr = torch.cat((Wh[U], Wh[V]), dim=1).to(self.device)
+
+        alpha = torch.zeros((n_nodes, n_nodes)).to(self.device)
+        alpha[U, V] = self.attention(edges_repr).squeeze(-1)
+        alpha = F.softmax(F.leaky_relu(alpha, negative_slope=self.input_slope), dim=1)
         
-        alpha = F.softmax(F.leaky_relu(coeffs, negative_slope=self.input_slope), dim=1)
-        
-        return self.activation(alpha.mm(Wh))
+        return torch.relu(alpha.mm(Wh))
 
-    def logits(self, features):
-        return self(features)
+class MultiHead(nn.Module):
+    def __init__(self, in_dim, out_dim, num_heads, device, average=False):
+        super(MultiHead, self).__init__()
+        self.average = average
+        self.heads = nn.ModuleList([AttentionHead(in_dim, out_dim, device) for _ in range(num_heads)])
 
-def main(args):
+    def forward(self, h):
+        head_outs = [attn_head(h) for attn_head in self.heads]
+        if self.average:
+            return torch.stack(head_outs, dim=2).mean(dim=2)
+        return torch.cat(head_outs, dim=1)
+
+class GAT(nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim, num_heads=4, device='cpu'):
+        super(GAT, self).__init__()
+        self.gat = nn.Sequential(
+            MultiHead(in_dim, hidden_dim, num_heads, device),
+            nn.ELU(inplace=True),
+            MultiHead(hidden_dim * num_heads, out_dim, num_heads, device, average=True)
+        )
+
+    def forward(self, h):
+        return self.gat(h)
+
+    def set_graph(self, g):
+        for module in self.gat:
+            if isinstance(module, MultiHead):
+                for head in module.heads:
+                    head.g = g
+
+def main(args, f1_scores=[]):
     # create the dataset
     train_dataset, test_dataset = LegacyPPIDataset(mode="train"), LegacyPPIDataset(mode="test")
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, collate_fn=collate_fn)
@@ -78,34 +106,36 @@ def main(args):
 
     # create the model, loss function and optimizer
     device = torch.device("cpu" if args.gpu < 0 else "cuda:" + str(args.gpu))
+
     if args.model == "BGM":
         model = BasicGraphModel(g=train_dataset.graph, n_layers=2, input_size=n_features,
-                            hidden_size=256, output_size=n_classes, nonlinearity=F.elu).to(device)
+                            hidden_size=args.hidden_dim, output_size=n_classes, nonlinearity=F.elu).to(device)
+    elif args.model == "GAT":
+        model = GAT(n_features, args.hidden_dim, n_classes, num_heads=args.n_heads, device=device).to(device)
     else:
-        model = GraphAttentionNetwork(input_F=n_features, output_F=n_classes, activation=F.elu).to(device)
+        raise AttributeError('invalid model name')
+
     loss_fcn = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
     # train and test
     if args.mode == "train":
-        train(model, loss_fcn, device, optimizer, train_dataloader, test_dataset)
+        train(model, loss_fcn, device, optimizer, train_dataloader, test_dataset, f1_scores=f1_scores)
         torch.save(model.state_dict(), MODEL_STATE_FILE)
-    model.load_state_dict(torch.load(MODEL_STATE_FILE))
+    model.load_state_dict(torch.load(MODEL_STATE_FILE, map_location=device))
     return test(model, loss_fcn, device, test_dataloader)
 
 
-def train(model, loss_fcn, device, optimizer, train_dataloader, test_dataset):
+def train(model, loss_fcn, device, optimizer, train_dataloader, test_dataset, f1_scores=[]):
     for epoch in range(args.epochs):
         model.train()
         losses = []
-        print("len loader", len(train_dataloader))
-        for batch, data in enumerate(train_dataloader):
-            print(batch)
+        for _, data in enumerate(train_dataloader):
             subgraph, features, labels = data
             features = features.to(device)
             labels = labels.to(device)
-            model.g = subgraph
-            logits = model.logits(features.float())
+            model.set_graph(subgraph.to(device))
+            logits = model(features.float())
             loss = loss_fcn(logits, labels.float())
             optimizer.zero_grad()
             loss.backward()
@@ -114,35 +144,36 @@ def train(model, loss_fcn, device, optimizer, train_dataloader, test_dataset):
         loss_data = np.array(losses).mean()
         print("Epoch {:05d} | Loss: {:.4f}".format(epoch + 1, loss_data))
 
-        if epoch % 5 == 0:
+        if epoch % 1 == 0:
             scores = []
-            for batch, test_data in enumerate(test_dataset):
+            for _, test_data in enumerate(test_dataset):
                 subgraph, features, labels = test_data
                 features = features.clone().detach().to(device)
                 labels = labels.clone().detach().to(device)
-                score, _ = evaluate(features.float(), model, subgraph, labels.float(), loss_fcn)
+                score, _ = evaluate(features.float(), model, subgraph, labels.float(), loss_fcn, device)
                 scores.append(score)
             print("F1-Score: {:.4f} ".format(np.array(scores).mean()))
-
+            f1_scores.append(np.array(scores).mean())
+    return f1_scores
 
 def test(model, loss_fcn, device, test_dataloader):
     print("\n-- Testing")
     test_scores = []
-    for batch, test_data in enumerate(test_dataloader):
+    for _, test_data in enumerate(test_dataloader):
         subgraph, features, labels = test_data
         features = features.to(device)
         labels = labels.to(device)
-        test_scores.append(evaluate(features, model, subgraph, labels.float(), loss_fcn)[0])
+        test_scores.append(evaluate(features, model, subgraph, labels.float(), loss_fcn, device)[0])
     mean_scores = np.array(test_scores).mean()
     print("F1-Score: {:.4f}".format(np.array(test_scores).mean()))
     return mean_scores
 
 
-def evaluate(features, model, subgraph, labels, loss_fcn):
+def evaluate(features, model, subgraph, labels, loss_fcn, device):
     with torch.no_grad():
         model.eval()
-        model.g = subgraph
-        output = model.logits(features.float())
+        model.set_graph(subgraph.to(device))
+        output = model(features.float())
         loss_data = loss_fcn(output, labels.float())
         predict = np.where(output.data.cpu().numpy() >= 0.5, 1, 0)
         score = f1_score(labels.data.cpu().numpy(), predict, average="micro")
@@ -157,12 +188,16 @@ def collate_fn(sample):
     return graph, features, labels
 
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode",  choices=["train", "test"], default="train")
     parser.add_argument("--gpu", type=int, default=-1, help="GPU to use. Set -1 to use CPU.")
     parser.add_argument("--epochs", type=int, default=250)
     parser.add_argument("--batch-size", type=int, default=2)
-    parser.add_argument("--model", type=str, default="BGM")
+    parser.add_argument("--model", type=str, default="GAT")
+    parser.add_argument("--learning_rate", type=float, default=5e-4)
+    parser.add_argument("--n_heads", type=int, default=4)
+    parser.add_argument("--hidden_dim", type=int, default=512)
     args = parser.parse_args()
     main(args)
